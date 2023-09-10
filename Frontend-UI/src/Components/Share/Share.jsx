7aae0f38537ac5c3995e7e9bbfee41241202e753
MRMutahir <mutahirkareem820@gmail>
2023-09-10 04:52:08 -0700

    files set

--- Frontend-UI/src/Components/Share/Share.jsx
+++ Frontend-UI/src/Components/Share/Share.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Share.css";
 import PermMediaIcon from "@mui/icons-material/PermMedia";
+import LabelIcon from "@mui/icons-material/Label";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
 function Share() {
   return (
     <div className="share">
@@ -16,10 +19,23 @@ function Share() {
         <div className="shareButtom">
           <div className="shareOptions">
             <div className="shareOption">
-              <PermMediaIcon className="shareIcon" />
+              <PermMediaIcon htmlColor="tomato" className="shareIcon" />
               <span className="shareOptionText">Photo or video</span>
             </div>
+            <div className="shareOption">
+              <LabelIcon htmlColor="blue" className="shareIcon" />
+              <span className="shareOptionText">Tags</span>
+            </div>
+            <div className="shareOption">
+              <LocationOnIcon htmlColor="green" className="shareIcon" />
+              <span className="shareOptionText">Location</span>
+            </div>
+            <div className="shareOption">
+              <EmojiEmotionsIcon htmlColor="goldenrod" className="shareIcon" />
+              <span className="shareOptionText">Feelings</span>
+            </div>
           </div>
+          <button className="share-btn">Share</button>
         </div>
       </div>
     </div>
