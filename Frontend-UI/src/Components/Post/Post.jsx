6d0d22def9301f540504d66cdff8cd79650d6440
MRMutahir <mutahirkareem820@gmail.com>
2023-09-11 02:02:48 +0500

    comment last

--- Frontend-UI/src/Components/Post/Post.jsx
+++ Frontend-UI/src/Components/Post/Post.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Post.css";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 function Post() {
   return (
     <div className="Post">
@@ -19,8 +21,24 @@ function Post() {
           </div>
           <MoreVertIcon />
         </div>
-        <div className="Post-Center"></div>
-        <div className="Post-Buttom"></div>
+        <div className="Post-Center">
+          <span className="p-c-text">
+            SALAM ! its my First post
+          </span>
+          <img className="post-image"  src="\assets\center-image.jpg" alt="" />
+        </div>
+        <div className="Post-Buttom">
+          <div className="p-b-left">
+            <FavoriteIcon htmlColor="red" />
+            <ThumbUpIcon htmlColor="lightblue" />
+            {/* <img src="" alt="" /> */}
+            <img src="" alt="" />
+            <span className="likeCount">20 people Like</span>
+          </div>
+          <div className="p-b-right">
+            <div className="p-comments-text">20 comments</div>
+          </div>
+        </div>
       </div>
     </div>
   );
