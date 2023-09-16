22e1abb55b5ba69ff96a3e4b10f40c9698ce6f6f
MRMutahir <mutahirkaeem820@gmail.com>
2023-09-16 09:11:54 -0700

    done ok

--- Frontend-UI/src/App.js
+++ Frontend-UI/src/App.js
@@ -1,13 +1,21 @@
 import "./App.css";
+// import { Route, Routes } from "react-router-dom";
+
 // import Profile from "./Pages/Profile/Profile";
-// import Home from "./Pages/Home/home";
+import Home from "./Pages/Home/home";
 // import Login from "../src/Pages/Login/Login";
-import Signup from "./Pages/Signup/Signup";
-{
-  /* <Home/> */
-}
+// import Signup from "./Pages/Signup/Signup";
+
 function App() {
-  return <Signup />;
+  return (
+    <Home/>
+  //   <Routes>
+  //   <Route path="/" element={<Home />} />
+  //   <Route path="/profile" element={<Profile />} />
+  //   <Route path="/signin" element={<Login />} />
+  //   <Route path="/signup" element={<Signup />} />
+  // </Routes>
+  );
 }
 
 export default App;
